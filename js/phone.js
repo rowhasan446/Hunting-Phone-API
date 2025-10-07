1386ede31b05980a46fec577defb379f73464a38
Rownak <rowhasan446@gmail.com>
2025-10-07 22:32:15 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,29 @@
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);
+}
+
+const displayPhones = phones =>{
+    // console.log(phones);
+    const phoneContainer = document.getElementById('Phone-container');
+    phones.forEach(phone=>{
+        console.log(phones);
+
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${phone.phone_name}</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>`;
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+loadPhone();
